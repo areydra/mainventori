c765f47aa424d5704cc7fe4f24097e8bb78238aa
Areydra Desfikriandre <areydra@gmail.com>
2023-12-31 18:52:22 +0700

    set minimum window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:mainventori/screens/home/index.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows) {
+    WindowManager.instance.setMinimumSize(const Size(1600, 1000));
+  }
+
   runApp(const MainApp());
 }
 
